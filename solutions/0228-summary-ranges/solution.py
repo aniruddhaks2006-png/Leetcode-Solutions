3ad436bb010e641b29c3707e9fa1f375364eeb53
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-05-23 15:07:18 +0000

    Sync LeetCode submission - Summary Ranges - Runtime - 0 ms (100.00%), Memory - 19.3 MB (63.87%)

---
+++ solutions/0228-summary-ranges/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+
+        if not nums:
+            return []
+
+        res = []
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+
+            if nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    res.append(str(start))
+                else:
+                    res.append(str(start) + "->" + str(nums[i - 1]))
+
+                start = nums[i]
+
+        # last range
+        if start == nums[-1]:
+            res.append(str(start))
+        else:
+            res.append(str(start) + "->" + str(nums[-1]))
+
+        return res
