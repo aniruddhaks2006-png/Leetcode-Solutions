51fd503ad1adcc7a2c94e813ada72db258cf2237
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-06-07 13:02:50 +0000

    Sync LeetCode submission - First Letter to Appear Twice - Runtime - 0 ms (100.00%), Memory - 19.2 MB (47.83%)

---
+++ solutions/2427-first-letter-to-appear-twice/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        l=[]
+        for i in s:
+            if i in l:
+                return i
+            else:
+                l.append(i)
+        return ""
