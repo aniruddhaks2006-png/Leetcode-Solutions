e586cd7d3b40a2f624346653f0d2bf04424c0932
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-06-09 04:41:00 +0000

    Sync LeetCode submission - Find Peak Element - Runtime - 0 ms (100.00%), Memory - 8.8 MB (26.49%)

---
+++ solutions/0162-find-peak-element/solution.c
@@ -0,0 +1,16 @@
+int findPeakElement(int* nums, int numsSize) {
+    int low=0;
+       int  high=numsSize-1;
+int mid;
+while(low<high){
+    mid=low+(high-low)/2;
+if(nums[mid]>nums[mid+1]){
+    high=mid;
+}
+else
+low=mid+1;
+}
+    return low;
+
+        
+}
