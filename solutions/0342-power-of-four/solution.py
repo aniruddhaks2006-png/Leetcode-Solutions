1fdc9a245f0eb46e983cbe53f304ad00f9d9d25f
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-06-11 12:59:37 +0000

    Sync LeetCode submission - Power of Four - Runtime - 0 ms (100.00%), Memory - 19 MB (97.71%)

---
+++ solutions/0342-power-of-four/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if (n>0) and (n&n-1)==0:
+            if (n-1)%3==0:
+                return True
+        return False
+        
