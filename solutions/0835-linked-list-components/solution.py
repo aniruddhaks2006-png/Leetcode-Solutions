ccd3bea394c64f38ae6ced43982bfb2c648e6381
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-06-19 02:33:42 +0000

    Sync LeetCode submission - Linked List Components - Runtime - 7 ms (42.52%), Memory - 24.3 MB (86.22%)

---
+++ solutions/0835-linked-list-components/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        s = set(nums)
+        count = 0
+        while head:
+            if head.val in s and (head.next is None or head.next.val not in s):
+                count += 1
+            head = head.next
+        return count
