8e31daa5e0cab2468c9ba3abd5dfbef50aa31707
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-06-21 15:50:57 +0000

    Sync LeetCode submission - Count of Matches in Tournament - Runtime - 0 ms (100.00%), Memory - 8.5 MB (43.99%)

--- solutions/1806-count-of-matches-in-tournament/solution.c
+++ solutions/1806-count-of-matches-in-tournament/solution.c
@@ -1,17 +1,5 @@
 
 
 int numberOfMatches(int n){
-    int sum=0;
-while (n>1){
-    
-    if (n%2==0){
-        sum+=n/2;
-        n=n/2;
-    }
-    else{
-        sum+=(n-1)/2;
-        n=(n-1)/2+1;
-    }
-}
-    return sum;
+return n-1;
 }
