b50a5a2c451a01466224a4e78daaf74da6f3c72e
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-08-17 17:52:04 +0000

    Sync LeetCode submission - Check Completeness of a Binary Tree - Runtime - 0 ms (100.00%), Memory - 19 MB (98.82%)

---
+++ solutions/0998-check-completeness-of-a-binary-tree/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        q=deque([root])
+        seen=False
+        while q:
+            node=q.popleft()
+            if node==None:
+                seen=True
+                continue
+            if seen:
+                return False
+            q.append(node.left)
+            q.append(node.right)
+        return seen
+            
