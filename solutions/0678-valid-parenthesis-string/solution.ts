235ad4c85c1145ec1d2bb891b43896443731141a
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-08-26 11:00:21 +0000

    Sync LeetCode submission - Valid Parenthesis String - Runtime - 0 ms (100.00%), Memory - 55.6 MB (42.10%)

---
+++ solutions/0678-valid-parenthesis-string/solution.ts
@@ -0,0 +1,34 @@
+function checkValidString(s: string): boolean {
+    const inside:number[]=[-1];
+    const star:number[]=[-1];
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="("){
+            inside.push(i);
+        }
+        else if(s[i]=="*"){
+            star.push(i);
+        }
+        else{
+            if(inside[inside.length-1]==-1 && star[star.length-1]==-1){
+                return false;
+            }
+            else if(inside[inside.length-1]!=-1)
+            {
+                inside.pop();
+            }
+            else if(star[star.length-1]!=-1){
+                star.pop();
+            }
+        }
+    }
+    while(inside[inside.length-1]!=-1 && star[star.length-1]!=-1){
+    if(inside.pop()>star.pop()){
+        return false;
+    }
+    }
+    if(inside.length>1){
+        return false;
+    }   
+    return true;
+    
+};
