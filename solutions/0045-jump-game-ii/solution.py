7153a706ed8c47e3d521048dbd01a53dca393003
aniruddhaks2006-png <aniruddhaks2006@gmail.com>
2026-09-07 16:31:30 +0000

    Sync LeetCode submission - Jump Game II - Runtime - 1627 ms (15.32%), Memory - 20.2 MB (34.48%)

---
+++ solutions/0045-jump-game-ii/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+     dp = [float('inf')] * len(nums)
+     dp[0] = 0
+
+     for i in range(len(nums)):
+           for j in range(i + 1, min(i + nums[i] + 1, len(nums))):
+                dp[j] = min(dp[j], dp[i] + 1)
+
+     return dp[-1]
+        
